bb1f08a212ca9bdae0e6e6d54f3fda01d42debab
cross <crosstown11223@yahoo.com>
2022-01-15 10:25:31 -0500

    created add method

--- src/practiceGit/Solution.java
+++ src/practiceGit/Solution.java
@@ -4,4 +4,8 @@ public class Solution {
     public static void main(String[] args) {
         System.out.println("Hello");
     }
+    public static int add(int num1, int num2){
+        int result = num1+num2;
+        return result;
+    }
 }
