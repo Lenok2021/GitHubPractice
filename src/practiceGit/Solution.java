5c501507357b460b53868f1936d4141aee28e418
cross <crosstown11223@yahoo.com>
2022-01-15 10:28:23 -0500

    created divide method

--- src/practiceGit/Solution.java
+++ src/practiceGit/Solution.java
@@ -12,4 +12,9 @@ public class Solution {
         int result=num1-num2;
         return result;
     }
+    public static int divide(int num1, int num2){
+        int result=num1/num2;
+        return result;
+    }
+
 }
